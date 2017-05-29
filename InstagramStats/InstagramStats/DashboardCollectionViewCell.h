3a8dd8cd3681f8a7655abae9e031d99a45e25674
Alex Mitchell <alex.mitchell@mac.com>
2017-05-29 11:29:44 -0400

    created custom DashboardCollectionViewCell and connected to prototype cell in storyboard

---
+++ InstagramStats/InstagramStats/DashboardCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  DashboardCollectionViewCell.h
+//  InstagramStats
+//
+//  Created by Alex Mitchell on 2017-05-29.
+//  Copyright © 2017 Alex Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic) NSArray *data;
+
+@end
