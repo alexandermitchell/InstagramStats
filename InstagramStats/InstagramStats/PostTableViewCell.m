f0f0dba9e439312ffd6f70c9050d306cd5fbf8b5
Marc Maguire <maguire.marc@gmail.com>
2017-05-31 17:26:01 -0400

    updated setter overrides in custom cell to properly calculate values

--- InstagramStats/InstagramStats/PostTableViewCell.m
+++ InstagramStats/InstagramStats/PostTableViewCell.m
@@ -32,12 +32,17 @@
 
 -(void)setPhotos:(NSOrderedSet<Photo *> *)photos {
     
-   
     //get and set minlike
+    self.minLike.text = [self getMinLikes:self.photos];
+    
     //get and set maxLike
+    self.maxLike.text = [self getMaxLikes:self.photos];
     
     //get and set minComment
+    self.minComment.text = [self getMinComments:self.photos];
+    
     //get and set maxComment
+    self.maxComment.text = [self getMaxComments:self.photos];
     
     
     _photos = photos;
@@ -46,15 +51,69 @@
 -(void)setDisplayPhoto:(Photo *)displayPhoto {
     
     //set postImageView
+    self.postImageView.image = [UIImage imageWithData:self.displayPhoto.image];
+    
     //set totalLikes
+    self.totalLikes.text = [NSString stringWithFormat:@"%hd",self.displayPhoto.likesNum];
     //set totalComments
     
+    self.totalComments.text = [NSString stringWithFormat:@"%hd",self.displayPhoto.commentsNum];
+    
     //set likeprogressbar (totalcomments / max comments)
+    self.likesProgress.progress = ((float)self.displayPhoto.likesNum / [self.maxLike.text floatValue]);
+   
     //set commentprogressbar (totalLikes / maxLikes)
+    self.commentsProgress.progress = ((float)self.displayPhoto.commentsNum / [self.maxComment.text floatValue]);
+
     
     
     _displayPhoto = displayPhoto;
 }
+
+-(NSString *)getMaxComments:(NSOrderedSet<Photo *> *)photos {
+    NSNumber *max = @(photos[0].commentsNum);
+    
+    for (int i = 0; i < photos.count; i++) {
+        if ([max compare:@(photos[i].commentsNum)] == NSOrderedAscending) {
+            max = @(photos[i].commentsNum);
+        }
+    }
+    return [max stringValue];
+}
+
+-(NSString *)getMinComments:(NSOrderedSet<Photo *> *)photos {
+    NSNumber *min = @(photos[0].commentsNum);
+    
+    for (int i = 0; i < photos.count; i++) {
+        if ([min compare:@(photos[i].commentsNum)] == NSOrderedDescending) {
+            min = @(photos[i].commentsNum);
+        }
+    }
+    return [min stringValue];
+}
+
+-(NSString *)getMaxLikes:(NSOrderedSet<Photo *> *)photos {
+    NSNumber *max = @(photos[0].likesNum);
+    
+    for (int i = 0; i < photos.count; i++) {
+        if ([max compare:@(photos[i].likesNum)] == NSOrderedAscending) {
+            max = @(photos[i].likesNum);
+        }
+    }
+    return [max stringValue];
+}
+
+-(NSString *)getMinLikes:(NSOrderedSet<Photo *> *)photos {
+    NSNumber *min = @(photos[0].likesNum);
+    
+    for (int i = 0; i < photos.count; i++) {
+        if ([min compare:@(photos[i].likesNum)] == NSOrderedDescending) {
+            min = @(photos[i].likesNum);
+        }
+    }
+    return [min stringValue];
+}
+
 
 - (void)awakeFromNib {
     [super awakeFromNib];
