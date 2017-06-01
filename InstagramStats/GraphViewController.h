8db3230fbe0e414b1af967d045ef134882f7c4bb
Adam Felix <adam.tyler.felix@gmail.com>
2017-06-01 14:06:44 -0400

    Add GraphViewController and ui is set for it

---
+++ InstagramStats/GraphViewController.h
@@ -0,0 +1,18 @@
+//
+//  GraphViewController.h
+//  
+//
+//  Created by atfelix on 2017-06-01.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "User.h"
+#import "User+CoreDataProperties.h"
+
+@interface GraphViewController : UIViewController
+
+@property (nonatomic) User *user;
+
+@end
